b02c55ce734185dd012ee6684f9288445c6df0d5
minyiky <mmcdonnell@hotmail.co.uk>
2023-11-19 22:28:36 +0000

    intial commit

--- pkg/point/point2d.go
+++ pkg/point/point2d.go
@@ -4,17 +4,17 @@ type Point2D struct {
 	values [2]int
 }
 
-func NewPoint2D(x, y int) *Point2D {
-	return &Point2D{
+func NewPoint2D(x, y int) Point2D {
+	return Point2D{
 		values: [2]int{x, y},
 	}
 }
 
-func (p *Point2D) Values() []int {
+func (p Point2D) Values() []int {
 	return p.values[:]
 }
 
-func (p *Point2D) X() int {
+func (p Point2D) X() int {
 	return p.values[0]
 }
 
@@ -22,10 +22,10 @@ func (p *Point2D) SetX(x int) {
 	p.values[0] = x
 }
 
-func (p *Point2D) Y() int {
+func (p Point2D) Y() int {
 	return p.values[1]
 }
 
-func (p *Point2D) SetY(y int) {
+func (p Point2D) SetY(y int) {
 	p.values[1] = y
 }
--- pkg/point/point3d.go
+++ pkg/point/point3d.go
@@ -4,17 +4,17 @@ type Point3D struct {
 	values [3]int
 }
 
-func NewPoint3D(x, y, z int) *Point3D {
-	return &Point3D{
+func NewPoint3D(x, y, z int) Point3D {
+	return Point3D{
 		values: [3]int{x, y, z},
 	}
 }
 
-func (p *Point3D) Values() []int {
+func (p Point3D) Values() []int {
 	return p.values[:]
 }
 
-func (p *Point3D) X() int {
+func (p Point3D) X() int {
 	return p.values[0]
 }
 
@@ -22,7 +22,7 @@ func (p *Point3D) SetX(x int) {
 	p.values[0] = x
 }
 
-func (p *Point3D) Y() int {
+func (p Point3D) Y() int {
 	return p.values[1]
 }
 
@@ -30,7 +30,7 @@ func (p *Point3D) SetY(y int) {
 	p.values[1] = y
 }
 
-func (p *Point3D) Z() int {
+func (p Point3D) Z() int {
 	return p.values[2]
 }
 
