f485a7e4627a2723fbf2af6c9bbdc107a8fa4353
minyiky <mmcdonnell@hotmail.co.uk>
2023-11-19 08:52:21 +0000

    added coverage

--- package/maths/maths_test.go
+++ package/maths/maths_test.go
@@ -10,12 +10,65 @@ import (
 
 func Test_Abs(t *testing.T) {
 	// -- Assemble --
-	in := -4.5
-	expected := -4.5
+	testCases := []struct {
+		Name     string
+		In       float64
+		Expected float64
+	}{
+		{ // Positive
+			Name:     "Positive",
+			In:       4.5,
+			Expected: 4.5,
+		}, { // Negative
+			Name:     "Negative",
+			In:       -4.5,
+			Expected: 4.5,
+		},
+	}
 
-	// -- Act --
-	actual := maths.Abs(in)
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.Name, func(t *testing.T) {
+			// -- Act --
+			result := maths.Abs(tc.In)
 
-	// -- Assert --
-	assert.Equal(t, expected, actual)
+			// -- Assert --
+			assert.Equal(t, tc.Expected, result)
+		})
+	}
+}
+
+func Test_Max(t *testing.T) {
+	// -- Assemble --
+	testCases := []struct {
+		Name     string
+		Slice    []float64
+		Expected float64
+	}{
+		{ // Single Value
+			Name:     "Single Value",
+			Slice:    []float64{4.5},
+			Expected: 4.5,
+		}, { // Multiple Values
+			Name:     "Multiple Values",
+			Slice:    []float64{4.5, -2.5, 3.5},
+			Expected: 4.5,
+		}, { // Empty Slice
+			Name:     "Empty Slice",
+			Slice:    []float64{},
+			Expected: 0,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.Name, func(t *testing.T) {
+			// -- Act --
+			actual := maths.Max(tc.Slice...)
+
+			// -- Assert --
+			assert.Equal(t, tc.Expected, actual)
+		})
+
+	}
 }
